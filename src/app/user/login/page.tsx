4ba929e697cadd47315869dbecbf5b60538ff56d
john <jhonfe649@gmail.com>
2024-01-15 00:57:35 -0500

    logging form center

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -3,8 +3,8 @@ import LoginForm from "@/components/user/loginForm/LoginForm";
 
 function Login() {
   return (
-    <div className="min-h-screen h-full felx  px-8 m-0 p-0 bg-black">
-      <div className="pt-16 sm:pt-44 pb-16">
+    <div className="min-h-screen h-full flex justify-center w-full items-center  px-8 m-0 p-0 bg-black">
+      <div className="w-full pt-16 pb-16">
         <LoginForm />
       </div>
     </div>
